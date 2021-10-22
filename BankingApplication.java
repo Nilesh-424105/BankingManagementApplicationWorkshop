8cd158f78447de1c414318e5ac78282a27ebd355
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-22 08:03:59 +0530

    ADDED: Welcome To Banking Management Application

---
+++ BankingApplication.java
@@ -0,0 +1,10 @@
+package com.banking.application;
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Banking Management Application Program");
+	}
+
+}
